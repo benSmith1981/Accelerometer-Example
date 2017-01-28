4f7f451b5af77708e55776311c9cdb7a4d149505
Ben Smith <benjaminsmith1981@gmail.com>
2017-01-28 21:18:46 +0100

    Fix the the compass!

--- Accelerometer-Example/CompassVC.swift
+++ Accelerometer-Example/CompassVC.swift
@@ -7,35 +7,20 @@
 //
 
 import UIKit
-import CoreMotion
+
+
 class CompassVC: UIViewController {
     @IBOutlet weak var uprightImage: UIImageView!
-
     //get cmmotion manager
-    let manager = CMMotionManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        if manager.isMagnetometerAvailable {
-            
-            moveCompass()
-        }
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    func moveCompass(){
-        manager.deviceMotionUpdateInterval = 0.01
-        manager.startMagnetometerUpdates(to: OperationQueue.main) {[weak self] (compassData, error) in
-            if let compass = compassData?.magneticField {
-                let rotation = atan2(compass.z, compass.y) - M_PI
-                self?.uprightImage.transform = CGAffineTransform(rotationAngle: CGFloat(rotation))
-            }
-        }
     }
 
     /*
